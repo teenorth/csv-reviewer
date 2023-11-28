01a1473c2e76ea14aa446e71027f85a0eb35946d
teenorth <taylornorth13@hotmail.com>
2023-11-28 21:55:16 +0000

    Created request schema validation

--- api/middleware/__init__.py
+++ api/middleware/__init__.py
@@ -13,30 +13,32 @@ from api.util import (
 )
 from flask import request
 from api import database as db
+from marshmallow import exceptions as m_exceptions
+import logging
 
 
 def jwt_required(func):
-    '''Will validate that the user has provided a valid JWT
+    """Will validate that the user has provided a valid JWT
     token before allowing the route method to called
-    '''
+    """
 
     @wraps(func)
     def wrapper(*args, **kwargs):
         token = get_bearer_jwt()
         if not token:
             return api_response(
-                message='Unauthorized access: No bearer token provided', status=401
+                message="Unauthorized access: No bearer token provided", status=401
             )
         if not jwt_decode(token):
             return api_response(
-                message='Unauthorized access: Token is invalid', status=401
+                message="Unauthorized access: Token is invalid", status=401
             )
         user_id = get_user_id()
         if user_id:
             found_user = db.user.show(object_id=user_id)
             if not found_user:
                 return api_response(
-                    message='Unauthorized access: User does not exist', status=401
+                    message="Unauthorized access: User does not exist", status=401
                 )
         return func(*args, **kwargs)
 
@@ -44,9 +46,9 @@ def jwt_required(func):
 
 
 def json_keys_required(keys):
-    '''Will validate that the JSON keys provided match and
+    """Will validate that the JSON keys provided match and
     have been sent over in the request body as JSON
-    '''
+    """
 
     def decorator(func):
         @wraps(func)
@@ -64,9 +66,9 @@ def json_keys_required(keys):
 
 
 def json_keys_accepted(keys):
-    '''Will validate that the JSON keys provided are the only
+    """Will validate that the JSON keys provided are the only
     keys provided in the request body as JSON
-    '''
+    """
 
     def decorator(func):
         @wraps(func)
@@ -82,18 +84,44 @@ def json_keys_accepted(keys):
 
 
 def has_role(roles):
-    '''Will validate that one of the roles provided are
+    """Will validate that one of the roles provided are
     present in the users JWT token
-    '''
+    """
 
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if not jwt_contains_role(roles):
                 return api_response(
-                    message='Unauthorized access: Invalid role', status=401
+                    message="Unauthorized access: Invalid role", status=401
                 )
             return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def request_schema(schema_object):
+    """Will validate that one of the roles provided are
+    present in the users JWT token
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                data = request.json
+                schema = schema_object()
+                schema.load(data)
+                return func(*args, **kwargs)
+            except m_exceptions.ValidationError as e:
+                logging.error(f"Failed to validate '{schema_object.__name__}': {e}")
+                return api_response(
+                    message="Failed to validate request",
+                    status=422,
+                    error=e.messages_dict,
+                )
 
         return wrapper
 
